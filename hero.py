43e1b7e6be413ddad9625600de8c5149362b2392
Maksat474 <konurbaevm3@gmail.com>
2023-10-09 23:01:22 +0600

    'hw2'

--- hero.py
+++ hero.py
@@ -20,10 +20,74 @@ class SuperHero:
     def __len__(self):
         return len(self.catchphrase)
 
-hero = SuperHero("Железный человек", "Айронмен", "Броня", 100, "Я – Железный человек!")
 
+hero = SuperHero("Железный человек", "Айронмен", "Броня", 100, "Я – Железный человек!")
 
 hero.display_name()
 hero.double_health()
 print(hero)
 print(f"Длина коронной фразы: {len(hero)}")
+
+
+class SuperHero:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+
+    def increase_hp(self):
+        self.hp **= 2
+        self.fly = True
+
+    def say_true(self):
+        print(f"{self.name} in the True_phrase: True")
+
+
+class AirHero(SuperHero):
+    def __init__(self, name, hp, damage):
+        super().__init__(name, hp)
+        self.damage = damage
+        self.fly = False
+
+
+class EarthHero(SuperHero):
+    def __init__(self, name, hp, damage):
+        super().__init__(name, hp)
+        self.damage = damage
+        self.fly = False
+
+
+class SpaceHero(SuperHero):
+    def __init__(self, name, hp, damage):
+        super().__init__(name, hp)
+        self.damage = damage
+        self.fly = False
+
+
+class Villain(SuperHero):
+    def __init__(self, name, hp, damage):
+        super().__init__(name, hp)
+        self.damage = damage
+        self.people = "monster"
+
+    def gen_x(self):
+        pass
+
+    def crit(self):
+        self.damage **= 2
+
+
+air_hero = AirHero("Airman", 100, 10)
+earth_hero = EarthHero("Earthman", 150, 15)
+space_hero = SpaceHero("Spaceman", 200, 20)
+villain = Villain("Evil", 100, 10)
+
+air_hero.increase_hp()
+earth_hero.increase_hp()
+space_hero.increase_hp()
+villain.crit()
+
+#
+print(f"{air_hero.name}'s HP: {air_hero.hp}, Fly: {air_hero.fly}")
+print(f"{earth_hero.name}'s HP: {earth_hero.hp}, Fly: {earth_hero.fly}")
+print(f"{space_hero.name}'s HP: {space_hero.hp}, Fly: {space_hero.fly}")
+print(f"{villain.name}'s HP: {villain.hp}, People: {villain.people}, Damage: {villain.damage}")
